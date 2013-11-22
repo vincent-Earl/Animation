6afd6920509cd6b0a2a0741084d517ea6426d110
vincent <jack.vincent.yu@gmail.com>
2013-11-22 15:16:49 +0800

    基本完成几种动画的效果，包括：移动，旋转，收缩，淡入淡出。

---
+++ src/com/example/animation/Activity4.java
@@ -0,0 +1,37 @@
+package com.example.animation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+/** 
+ * 文件名称:   Activity4.java 
+ * 功能描述:  
+ * 版本信息:   Copyright (c)2013 
+ * 公司信息:   瑜旭网络
+ * 开发人员:   yuyejiang
+ * 版本日志:   1.0 
+ * 创建时间:   2013年11月22日 上午11:34:10 
+ * 
+ * 修改历史: 
+ * 时间         开发者      版本号    修改内容 
+ * ------------------------------------------------------------------ 
+ * 2013年11月22日   yuyejiang      1.0         1.0 Version 
+ */
+public class Activity4 extends Activity {
+	private List<String> dataList = new ArrayList<String>();
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout_activity4);
+		ListView listView = (ListView)findViewById(R.id.listview);
+		for (int i = 0; i < 15; i++) {
+			dataList.add("app "+ i );
+		}
+		listView.setAdapter(new MyAdapter(this, dataList));
+	}
+}
--- src/com/example/animation/MainActivity.java
+++ src/com/example/animation/MainActivity.java
@@ -1,23 +1,64 @@
 package com.example.animation;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnClickListener{
+	private Button btn1,btn2,btn3,btn4,btn5;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		//试试从git-hub上面拉下来的汉字是否正确显示
-	}
+		
+		btn1 = (Button)findViewById(R.id.button1);
+		btn2 = (Button)findViewById(R.id.button2);
+		btn3 = (Button)findViewById(R.id.button3);
+		btn4 = (Button)findViewById(R.id.button4);
+		btn5 = (Button)findViewById(R.id.button5);
+		
+		btn1.setOnClickListener(this);
+		btn2.setOnClickListener(this);
+		btn3.setOnClickListener(this);
+		btn4.setOnClickListener(this);
+		btn5.setOnClickListener(this);
+		
+	} 
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.button1:
+			startActivity(new Intent(this, Activity1.class));
+			break;
+		case R.id.button2:
+			startActivity(new Intent(this, Activity2.class));
+			break;
+		case R.id.button3:
+			startActivity(new Intent(this, Activity3.class));
+			break;
+		case R.id.button4:
+			startActivity(new Intent(this, Activity4.class));
+			break;
+		case R.id.button5:
+			startActivity(new Intent(this, Activity5.class));
+			break;
+
+		default:
+			break;
+		}
 	}
 
 }
